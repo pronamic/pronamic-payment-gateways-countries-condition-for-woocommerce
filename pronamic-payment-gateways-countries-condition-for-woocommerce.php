e6e2c9ebc270e6a1406e79ffd45775cd4575d300
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-10-31 14:36:19 +0100

    Update pronamic-payment-gateways-countries-condition-for-woocommerce.php

--- pronamic-payment-gateways-countries-condition-for-woocommerce.php
+++ pronamic-payment-gateways-countries-condition-for-woocommerce.php
@@ -8,16 +8,18 @@
  * 
  * @wordpress-plugin
  * Plugin Name: Pronamic Payment Gateways Countries Condition for WooCommerce
+ * Plugin URI: https://www.pronamic.shop/product/pronamic-payment-gateways-countries-condition-for-woocommerce/
  * Description: This plugin allows you to specify the countries in which each WooCommerce payment gateway is available.
- * Version:     1.0.0
- * Author:      Pronamic
- * Author URI:  https://www.pronamic.eu/
+ * Version: 1.0.0
+ * Requires at least: 6.1
+ * Requires PHP: 8.0
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ * License: Proprietary
+ * License URI: https://www.pronamic.shop/product/pronamic-payment-gateways-countries-condition-for-woocommerce/
  * Text Domain: pronamic-payment-gateways-countries-condition-for-woocommerce
  * Domain Path: /languages/
- * License:     Proprietary
- * License URI: https://www.pronamic.shop/product/pronamic-payment-gateways-countries-condition-for-woocommerce/
- * Update URI:  https://wp.pronamic.directory/plugins/pronamic-payment-gateways-countries-condition-for-woocommerce/
- *
+ * Update URI: https://wp.pronamic.directory/plugins/pronamic-payment-gateways-countries-condition-for-woocommerce/
  * WC requires at least: 8.0
  * WC tested up to: 8.0
  */
@@ -46,3 +48,5 @@ add_action(
 );
 
 \Pronamic\WooCommercePaymentGatewaysCountriesCondition\Plugin::instance()->setup();
+
+\Pronamic\WordPress\Updater\Plugin::instance()->setup();
